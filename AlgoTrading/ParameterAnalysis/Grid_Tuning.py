1139f59fe065be45afb8a69e023e93f357cbf4d9
Junhui <bizjun@nus.edu.sg>
2021-05-06 11:27:37 +0800

    Tunning

--- AlgoTrading/ParameterAnalysis/Grid_Tuning.py
+++ AlgoTrading/ParameterAnalysis/Grid_Tuning.py
@@ -5,17 +5,12 @@ BOLL_LOWER = "boll_lower"
 CLOSE = "Close"
 TIMESTAMP = "TimeStamp"
 
-
-# N = 24 * 60
-# M = 2
-# R_DECREASING = 0.005
-# R_INCREASING = 0.005
-# last_price = 0
 BEGINNING_CASH = 100000
 FEE = 0.005
 records = []
 profit = 0
 start_index = 0
+N_GRID = 20
 
 
 def calc_RSV(prices, INTERVALS_FOR_RSV):
@@ -53,6 +48,7 @@ def generate_records_df(records):
             "Profit",
             "LowerBound",
             "UpperBound",
+            "Rate",
             "Hour",
         ],
     )
@@ -60,20 +56,29 @@ def generate_records_df(records):
     return records_df
 
 
+def get_rate(lower_bound, upper_bound):
+    for n in range(N_GRID, 5, -5):
+        rate = pow(upper_bound / lower_bound, 1 / (n - 1)) - 1
+        if rate > FEE:
+            return rate
+    return FEE + 0.001
+
+
 def transact(
-    data, R_DECREASING, R_INCREASING, n=0, cash=BEGINNING_CASH, last_price=0,
+    data, n=0, cash=BEGINNING_CASH, last_price=0,
 ):
     for index, row in data.iterrows():
         time = row[TIMESTAMP]
         price = row[CLOSE]
         upper_bound = row[BOLL_UPPER]
         lower_bound = row[BOLL_LOWER]
+        rate = get_rate(lower_bound, upper_bound)
         trading_buying_price = price * (1 + FEE)
         trading_selling_price = price * (1 - FEE)
 
         if n == 0 and (
             trading_buying_price < upper_bound
-            or trading_buying_price < last_price * (1 - R_DECREASING)
+            or trading_buying_price < last_price * (1 - rate)
         ):
             if trading_buying_price > lower_bound:
                 last_price = trading_buying_price
@@ -90,13 +95,14 @@ def transact(
                             round(profit),
                             lower_bound,
                             upper_bound,
+                            rate,
                             time,
                         ]
                     )
 
         elif n > 0 and (
             trading_selling_price > lower_bound
-            or trading_selling_price > last_price * (1 + R_INCREASING)
+            or trading_selling_price > last_price * (1 + rate)
         ):
             if (
                 trading_selling_price < upper_bound
@@ -115,6 +121,7 @@ def transact(
                         round(profit),
                         lower_bound,
                         upper_bound,
+                        rate,
                         time,
                     ]
                 )
@@ -128,12 +135,7 @@ def get_final_profit(df):
 
 
 def grid_trading(
-    data,
-    INTERVALS_FOR_RSV,
-    INTERVAL_FOR_BOUNDS,
-    SD_PARAMETER,
-    R_INCREASING,
-    R_DECREASING,
+    data, INTERVALS_FOR_RSV, INTERVAL_FOR_BOUNDS, SD_PARAMETER,
 ):
     RSV = calc_RSV(data["Close"], INTERVALS_FOR_RSV)
     data["RSV"] = RSV
@@ -142,21 +144,17 @@ def grid_trading(
     data["boll_lower"] = lower
     data = data.dropna()
     data.reset_index(drop=True, inplace=True)
-    records = transact(data, R_INCREASING=R_INCREASING, R_DECREASING=R_DECREASING)
+    records = transact(data)
     records_df = generate_records_df(records)
     final_profit = get_final_profit(records_df)
-    return final_profit
+    return records_df, final_profit
 
 
 FILENAME = "Binance_BTC_1m.csv"
 data = pd.read_csv(FILENAME)
-final_profit = grid_trading(
-    data,
-    INTERVALS_FOR_RSV=9,
-    INTERVAL_FOR_BOUNDS=24 * 60,
-    SD_PARAMETER=3,
-    R_INCREASING=0.005,
-    R_DECREASING=0.006,
+records_df, final_profit = grid_trading(
+    data, INTERVALS_FOR_RSV=9, INTERVAL_FOR_BOUNDS=24 * 60, SD_PARAMETER=3,
 )
+# records_df.to_excel("Transactions.xlsx")
 
-print(final_profit)
+# print(final_profit)
